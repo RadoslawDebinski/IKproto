3619c8c6f9f07e139d2390475b15702854627278
RadoslawDebinski <83645103+RadoslawDebinski@users.noreply.github.com>
2023-03-17 19:39:05 +0100

    Acceptable calculations for angles q1, q2, q3 and q5. For angle q4 further theory is required.

--- main.py
+++ main.py
@@ -67,9 +67,9 @@ if __name__ == '__main__':
     # h0to5 = np.matmul(iCsc(0, 0, 450, 0, 0, np.pi), fCsc(-1925, -140, 1395.737, 0, np.pi, 0))
 
     h0toB = iCsc(0, 0, 450, 0, 0, np.pi)
-    hBto5 = fCsc(-1985.483, -676.947, 2543.036, -np.pi - 1 * np.pi / 12, -np.pi / 2 - np.pi / 4, np.pi/2 + 7 * np.pi / 12)
-    z5axisCorr = np.append(np.append(rotZ(0), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)  # rotZ(np.pi/6)
-    hBto5 = hBto5.dot(z5axisCorr)
+    hBto5 = fCsc(-804.847, 303.021, 2438.528, np.pi, 0, np.pi/6)
+    q5 = np.array([np.pi/6])
+    h0to5 = h0toB.dot(hBto5)
 
     # # # TESTS STEP 1
     # # # SIMPLE TEST FOR "Forward coordinate system conversion - fCsc" - PASS!
@@ -84,53 +84,47 @@ if __name__ == '__main__':
     # EXAMPLE OF INPUT DATA 1
     # h0toB(0, 0, 450, 0, 0, np.pi)
     # hBto5(-1925, -140, 1395.737, np.pi, 0, 0)
-    # z5axisCorr = np.append(np.append(rotZ(0), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)
     # EXAMPLE OF OUTPUT DATA 1
     # pw = -1925, 10, 1705.737 - PASS!
 
     # EXAMPLE OF INPUT DATA 2
     # h0toB(0, 0, 0, 0, 0, 0)
     # hBto5(-2194.111, -140, 2016.32, np.pi, -np.pi / 2, 0)
-    # z5axisCorr = np.append(np.append(rotZ(0), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)
     # EXAMPLE OF OUTPUT DATA 2
     # pw = -1884.111, 10, 2016.32 - PASS!
 
     # EXAMPLE OF INPUT DATA 3
     # h0toB(0, 0, 0, 0, 0, 0)
-    # hBto5(-804.847, 303.021, 2438.528, np.pi, 0, 0)
-    # z5axisCorr = np.append(np.append(rotZ(np.pi/6), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)
+    # hBto5(-804.847, 303.021, 2438.528, np.pi, 0, np.pi/6)
     # EXAMPLE OF OUTPUT DATA 3
     # pw = -729.847, 432.924, 2748.528 - PASS!
 
     # EXAMPLE OF INPUT DATA 4
     # h0toB(0, 0, 0, 0, 0, 0)
-    # hBto5(-140, 1764.23, 795.737, np.pi, 0, 0)
-    # z5axisCorr = np.append(np.append(rotZ(np.pi/2), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)
-    # EXAMPLE OF OUTPUT DATA 3
+    # hBto5(-140, 1764.23, 795.737, np.pi, 0, np.pi/2)
+    # EXAMPLE OF OUTPUT DATA 4
     # pw = 10, 1764.23, 1105.737 - PASS!
 
     # EXAMPLE OF INPUT DATA 5
     # h0toB(0, 0, 0, 0, 0, 0)
     # hBto5(-1057.125, -1255.115, 666.506, -np.pi/2, -np.pi / 2 -np.pi / 4, np.pi/2)
-    # z5axisCorr = np.append(np.append(rotZ(0), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)
-    # EXAMPLE OF OUTPUT DATA 3
+    # EXAMPLE OF OUTPUT DATA 5
     # pw = -943.988, -929.845, 666.506 - PASS!
 
     # EXAMPLE OF INPUT DATA 6
     # h0toB(0, 0, 0, 0, 0, 0)
     # hBto5(-1985.483, -676.947, 2543.036, -np.pi - 1 * np.pi / 12, -np.pi / 2 - np.pi / 4, np.pi/2 + 7 * np.pi / 12)
-    # z5axisCorr = np.append(np.append(rotZ(0), [[0], [0], [0]], axis=1), [[0, 0, 0, 1]], axis=0)
-    # EXAMPLE OF OUTPUT DATA 3
+    # EXAMPLE OF OUTPUT DATA 6
     # pw = -1812.572, -475.324, 2323.833
 
-
-
     # pw DATA
-    werY = np.array([[hBto5[0][1]], [hBto5[1][1]], [hBto5[2][1]]])
-    werZ = np.array([[hBto5[0][2]], [hBto5[1][2]], [hBto5[2][2]]])
-    tran = np.array([[hBto5[0][3]], [hBto5[1][3]], [hBto5[2][3]]])
+    werY = np.array([[h0to5[0][1]], [h0to5[1][1]], [h0to5[2][1]]])
+    werZ = np.array([[h0to5[0][2]], [h0to5[1][2]], [h0to5[2][2]]])
+    tran = np.array([[h0to5[0][3]], [h0to5[1][3]], [h0to5[2][3]]])
 
     # # pw COMPUTE and DH PARAMS
+    L1 = DHPARAMS[1][1]
+    L2 = DHPARAMS[2][1]
     L3 = DHPARAMS[3][0]
     L4 = DHPARAMS[3][1]
 
@@ -138,4 +132,24 @@ if __name__ == '__main__':
     tranWriZ = L4*werZ
 
     pw = tran + tranWriY - tranWriZ
-    print(pw)
+
+    xp, yp, zp = pw[0], pw[1], pw[2]
+    r = np.sqrt(np.square(xp) + np.square(yp))
+    s = np.sqrt(np.square(r) + np.square(zp))
+
+    alpha = np.arctan2(zp, r)
+    beta = np.arccos((np.square(s) + np.square(L1) - np.square(L2))/(2 * s * L1))
+    gamma = np.arccos((np.square(L1) + np.square(L2) - np.square(s))/(2 * L1 * L2))
+
+    q1 = np.arctan2(yp, xp)
+    q2 = np.pi / 2 - alpha - beta
+    q3 = np.pi - gamma
+
+    print(f"angle q1 = {np.round(q1[0] * 180 / np.pi, 5)}°")
+    print(f"angle q2 = {np.round(q2[0] * 180 / np.pi, 5)}°")
+    print(f"angle q3 = {np.round(q3[0] * 180 / np.pi, 5)}°")
+    # print(f"angle q4 = {np.round(180 / np.pi, 5)}°")
+    print(f"angle q5 = {np.round(q5[0] * 180 / np.pi, 5)}°")
+
+
+
